d5d8e9d385fc4b9d120671b6e8bd04babf952255
Viktor <victorharasymiv97@gmail.com>
2025-01-29 00:32:31 +0100

    add about section

--- src/public/js/portfolio.js
+++ src/public/js/portfolio.js
@@ -3,6 +3,8 @@
 const galleryBox = document.querySelector('#portfolio--section');
 const addChildetoPortfolio = document.querySelector('.portfolio--list');
 
+const targetImg = document.querySelector('.portfolio--item_previewScreen');
+
 /*  Skills array  */
 const skills = {
     HTML : "html.svg",
@@ -10,6 +12,10 @@ const skills = {
     JS : "node-js.svg",
     React :"react.png",
     Redux: "redux.svg",
+    TypeScript : "typescript.svg",
+    Figma : "figma.svg",
+    MoongoDB :"mongoDB.png"
+
 }
 
 const skillsValue = Object.values(skills);
@@ -23,8 +29,10 @@ const portfolioItems = [
         previewScreen: "createx.jpg",
         data: "December 2024",
         title: "Createx",
+        url : "https://viktorharasymiv.github.io/createX/",
+        fullImage : "image.jpg",
         about: "Many complex components were implemented in the project: credit calculators, multi-stage mortgage application forms, asynchronous filter of bank services, custom maps (REST API YandexMap) with ATMs and office points.",
-        skills: [skillsValue[0] , skillsValue[1] , skillsValue[2] , skillsValue[3], skillsValue[4]]
+        skills: [skillsValue[0] , skillsValue[1] , skillsValue[2] , skillsValue[3], skillsValue[4] , skillsValue[6] , skillsValue[7] ]
 
     }],
     [{
@@ -32,18 +40,19 @@ const portfolioItems = [
         previewScreen: "interno.jpg",
         data: "September 2024",
         title: "Interno",
-        about: "(REST API YandexMap) with ATMs and office points.",
-        skills: [skillsValue[0] , skillsValue[1] , skillsValue[3]] ,
+        url : "https://viktorharasymiv.github.io/createX/",
+        about: "Minimalistic,based on SPA with integration of several technologies: Vite, TypeScript, React, React Router, Redux, Redux Toolkit, Redux Persist, Formik, Yup, classNames, PostCSS, Vite SVG icon, Vitest etc.",
+        skills: [skillsValue[0] , skillsValue[1] , skillsValue[3] , skillsValue[6] , skillsValue[7] ] ,
     }],
     [{
         id: 3,
         previewScreen: "shop.jpg",
-        data: "September 2024",
-        title: "Interno",
-        about: "(REST API YandexMap) with ATMs and office points.",
-        skills: [skillsValue[0] , skillsValue[1] , skillsValue[3]] ,
+        data: "January 2025",
+        title: "Сlothing store",
+        url : "https://viktorharasymiv.github.io/createX/",
+        about: "Multi-page online store.",
+        skills: [skillsValue[0] , skillsValue[1] , skillsValue[2] , skillsValue[3] , skillsValue[4] , skillsValue[6] , skillsValue[7] ]  ,
     }]
-    
 ]
 
 /* Function for mark up list item to portfolio */
@@ -52,16 +61,16 @@ function addItems(id) {
     let addPortfolioItem = portfolioItems[id].map(elem =>
         `<li class="portfolio--item">
               <a onclick="showFullScreenImg(event, ${id})" href="" class="portfolio--item_previewScreen">
-                 <img src="${elem.previewScreen}" data-id="${id}" />
+                 <img src="${elem.previewScreen}" data-id="${elem.fullImage}" />
               </a>
               <div class="portfolio--item_content">
                  <span class="portfolio--item_data">${elem.data}</span>
-                 <h4 class="portfolio--item_title">${elem.title}</h4>
+                 <a href=${elem.url} target="_blank"><h4 class="portfolio--item_title">${elem.title}</h4> </a>
                  <p class="portfolio--item_about">${elem.about}</p>
                  <ul class="portfolio--item_icons_list">
                    ${elem.skills
                     .map(skill =>
-                   `<li class="portfolio--item_link"><img src="icons/${skill}" width="22px" height="22px" class="portfolio--item_skill" /></li>`)
+                   `<li class="portfolio--item_link"><img src="/icons/${skill}" width="22px" height="22px" class="portfolio--item_skill" /></li>`)
                     .join('')}
                  </ul>
               </div>
@@ -73,35 +82,41 @@ function addItems(id) {
     return addPortfolioItem.about;
 }
 
-function showFullScreenImg(event, id) {
+function showFullScreenImg(event) {
+
     event.preventDefault()
-    addChildetoPortfolio.addEventListener('click', (event) =>  {
 
-           const selectedItem = portfolioItems[id][0];
-        
-        console.log(selectedItem);
-        
-        const previewScreenItem = event.target;
+    const previewScreenItem = event.target;
 
-        if (previewScreenItem.nodeName !== 'IMG') {
-            return;
-          }
+    const imageData =  event.target.dataset;
+
+    console.log(imageData);
+    
+
+    if (previewScreenItem.nodeName !== 'IMG') { return; }
+
             const markUpScreen = `
             <div class="portfolio--modal">
-              <button onClick="closeModal()" class="close_modal">X</button>
-                <div class="portfolio--overlay">
-                  <img onClick="closeModal()" src="${previewScreenItem.src}" />        
-                </div> `
+              <button onClick="closeModal()" class="close_modal">
+            </button>
+            
+            <div class="portfolio--overlay">
+            <div class="image_scroll_box">
+              <img onClick="closeModal()" src="${imageData.id}" class="portfolio--overlay_image" /> 
+            </div>      
+            </div>
+                                 `
 
-    
     const existingModal = document.querySelector(".portfolio--modal");
 
     if (existingModal) {
+
         existingModal.remove();
+
     }
     
     galleryBox.insertAdjacentHTML("beforeend", markUpScreen);
-})
+
 };
 
 function closeModal() {
@@ -114,6 +129,7 @@ function closeModal() {
 
 
 
+/* Initial function */ 
 
 for(let i = 0; i <= portfolioItems.length - 1; i++ ) {
     addItems(i);
