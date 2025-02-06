673154ce1147e38a4d61cf27d0fcc07bb9d947c9
Viktor <victorharasymiv97@gmail.com>
2025-02-06 01:49:12 +0100

    php mailer

---
+++ src/public/js/formValidator.js
@@ -0,0 +1,30 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded' , function () {
+
+    const form = document.querySelector('.contact--form');
+
+    form.addEventListener('submit' , formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        // let error = formValidate(form);
+        let formData = new FormData(form);
+        console.log(formData);
+
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if(response.ok) {
+                form.reset();
+            }
+        }
+    })
+
+
+    // function formValidate(form) {
+    //     let error = 0;
+    //     let formReq = document.querySelector('._req');
+    // }
