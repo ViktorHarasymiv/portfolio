b8fa36b63947e3f5ccb982a475669ff0243f1feb
Viktor <victorharasymiv97@gmail.com>
2025-02-07 00:38:50 +0100

    fix php

--- src/public/sendmail.php
+++ src/public/sendmail.php
@@ -1,12 +1,23 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php'
+require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 
 try {
+
+   //Server settings
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+$mail->isSMTP();                                            //Send using SMTP
+$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->Username   = 'victorharasymiv97@gmail.com';                     //SMTP username
+$mail->Password   = 'dcum cxqr stzj wbjp';                               //SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+$mail->Port       = 465;                                    
 
 $mail ->CharSet = 'UTF - 8';
 $mail ->setLanguage('ua', 'phpmailer/language/');
@@ -33,14 +44,8 @@ if(trim(!empty($_POST['message']))) {
 $mail->Body = $body;
 
 
-if (!$mail->send()) {
-    $message = 'Помилка !';
-} else {
-    $message = 'Відправлено !';
+$mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
-$response = ['message' => $message];
-
-header('Content-type: application/json');
-echo json_encode($response);
-?>
