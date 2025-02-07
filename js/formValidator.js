d668b6b8b69b7469b06d47c331b49dce288a0946
ViktorHarasymiv <115593029+ViktorHarasymiv@users.noreply.github.com>
2025-02-07 01:19:14 +0000

    Deploying to gh-pages from @ ViktorHarasymiv/portfolio@d1b56ce3e782f6e495b56174271ad08d34a33eff 🚀

--- js/formValidator.js
+++ js/formValidator.js
@@ -1,25 +1,25 @@
 'use strict'
 
-// document.addEventListener('DOMContentLoaded' , function () {
+document.addEventListener('DOMContentLoaded' , function () {
 
-//     const form = document.querySelector('.contact--form');
+    const form = document.querySelector('.contact--form');
 
-//     form.addEventListener('submit' , formSend);
+    form.addEventListener('submit' , formSend);
 
-//     async function formSend(e) {
-//         e.preventDefault();
+    async function formSend(e) {
+        e.preventDefault();
 
-//         // let error = formValidate(form);
-//         let formData = new FormData(form);
-//         console.log(formData);
+        // let error = formValidate(form);
+        let formData = new FormData(form);
+        console.log(formData);
 
-//             let response = await fetch('sendmail.php' , {
-//                 method: 'POST',
-//                 body: formData,
-//                 mode: 'no-cors'
-//             });
-//             if(response.ok) {
-//                 form.reset();
-//             }
-//         }
-//     })
+            let response = await fetch('sendmail.php' , {
+                method: 'POST',
+                body: formData,
+                mode: 'no-cors'
+            });
+            if(response.ok) {
+                form.reset();
+            }
+        }
+    })
