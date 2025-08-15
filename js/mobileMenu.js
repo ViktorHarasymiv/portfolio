39919c5403f78c05abf5a5008b78d91b3143cb28
ViktorHarasymiv <115593029+ViktorHarasymiv@users.noreply.github.com>
2025-08-15 22:36:42 +0000

    Deploying to gh-pages from @ ViktorHarasymiv/portfolio@288f9eb580a561371a6c95a81bebed1710f8a23d 🚀

---
+++ js/mobileMenu.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('.ham4');
+const activePanel = document.querySelector('.header_navigation');
+
+function openModal() {
+  activePanel.classList.toggle('is-visible');
+  menuButton.classList.toggle('active');
+}
